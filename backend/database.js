de3a336338128e3594a4cbbd8187e3dfbe2ed339
Diegorodri14 <diegorodrigueza14@gmail.com>
2026-06-15 07:57:08 -0600

    feat: Agregacion de estructura de mi backend

---
+++ backend/database.js
@@ -0,0 +1,17 @@
+import mongoose, { disconnect } from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/HospitalRosales")
+
+const connection = mongoose.Connection
+
+connection.on("open", () =>{
+    console.log("base conectada")
+});
+
+connection.on( disconnect , () =>{
+    console.log("base desconectada")
+});
+
+connection.on("error", (error) =>{
+    console.log("Error en la conexion a la base")
+});
---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import "./database.js"
+
+async function main (params) {
+    app.listen(4000)
+    console.log("Servidor en el puerto 4000")
+}
+
+main();
